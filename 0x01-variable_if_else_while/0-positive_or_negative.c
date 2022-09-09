e780fc36aea81645ecd10f57a2717698945be4fb
heefCodes <abdulkareemtoyin277@gmail.com>
2022-09-09 07:48:50 -0700

    updated

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  * Return: Always 0 (Success)
  */
 
@@ -21,10 +21,15 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n)
+		printf("%d is negative\n", n);
 	}
+
 	return (0);
+
 }
+
+
