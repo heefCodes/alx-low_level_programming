dc877bef028b67ea9f90dd512772462d49d3a9b8
heefCodes <abdulkareemtoyin277@gmail.com>
2022-09-15 11:02:17 -0700

    printing triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "mian.h"
+
+/**
+ * print_triangle - print triangle with #
+ * @size: size of triangle to draw
+ */
+
+void print_triangle(int size)
+{
+	int height;
+	int width;
+	int draw;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= (size - height); width++)
+			_putchar(' ');
+		_putchar('\n');
+	}
+}
