4635e81df44746519f9304f0210362bd0989abf8
heefCodes <abdulkareemtoyin277@gmail.com>
2022-10-04 10:50:03 -0700

    refactor

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (*(s1 + j))
+	while (*(s1 + i))
+		len1++, i++;
+	while (*(s2 + j))
 		len2++, j++;
 	len2++;
 
