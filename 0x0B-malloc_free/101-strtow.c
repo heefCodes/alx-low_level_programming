cbf6f51727c3fc34ef218be09dc6768171586c1e
heefCodes <abdulkareemtoyin277@gmail.com>
2022-10-04 11:32:19 -0700

    refactor

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,29 +5,29 @@
 
 
 /**
- * strtow - concatenate arguments
+ * strtow - concatenates arguments
  * @str: string to be splitted
  * Return: a pointer to array of string
  */
 
 char **strtow(char *str)
 {
-	char *arr = NULL;
+	char *arra = NULL;
 	unsigned int i = 0, j = 0, k;
 
 	if (strncmp(str, "", 1) || str == NULL)
 		return (NULL);
-	arr = malloc((i + j + 1) * sizeof(char));
-	if (arr == NULL)
+	arra = malloc((i + j + 1) * sizeof(char));
+	if (arra == NULL)
 		return (NULL);
 	for (k = 0; k < i; k++)
-		arr[k] = str[k];
+		arra[k] = str[k];
 	i = k;
 	for (k = 0; k < j; k++)
 	{
 		arra[i] = str[k];
 		i++;
 	}
-	arr[i] = '\0';
+	arra[i] = '\0';
 	return (NULL);
 }
