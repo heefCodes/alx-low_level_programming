14b02b51734a6996782bb5ffa9b51789a4ea4bee
heefCodes <abdulkareemtoyin277@gmail.com>
2022-10-26 07:39:24 +0000

    fixed betty issue

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - ckeck the code
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
