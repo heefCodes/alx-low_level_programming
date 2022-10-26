fb0acc3c5e6ffef301eeaba4a2683337f133d89f
heefCodes <abdulkareemtoyin277@gmail.com>
2022-10-26 07:55:59 +0000

    print prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* prime_number - recursively divide by higher divisor, skip even numbers
+* @n: number to check
+* @divisor: divisor
+* Return: 1 if prime, 0 if not, or recursive function call
+*/
+
+int prime(int n, int divisor)
+{
+	if (n == divisor)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (prime(n, divisor + 1));
+}
+
+/**
+* is_prime_number - checks if prime
+* @n: number to check
+* Return: 1 if prime, 0 if not
+*/
+
+int is_prime_number(int n)
+{
+	int divisor = 3;
+
+	if (n % 2 == 0 || n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+
+	return (prime(n, divisor));
+}
