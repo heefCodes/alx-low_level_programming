0f0a4a5be5c77d4722928931d6ec423975910ef8
heefCodes <abdulkareemtoyin277@gmail.com>
2022-11-10 10:01:55 +0000

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,11 @@ void more_numbers(void)
 	{
 		for (num = '0'; num <= 14; num++)
 		{
-			_putchar(num);
+			if (num / 10 > 0)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0')
+
 		}
-		_putchar(counter);
 	}
 	_putchar('\n');
 }
