7306b808c5f2861a0e0a0e9fb0402705a63957d0
heefCodes <abdulkareemtoyin277@gmail.com>
2022-11-10 10:04:54 +0000

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * more_numbers - funtion to print 10 times the numbers, from 0 to 14
- * Return: 012345678911121314
  */
 
 void more_numbers(void)
@@ -10,9 +9,9 @@ void more_numbers(void)
 	int counter;
 	int num;
 
-	for (counter = 0; counter <= 10; counter++)
+	for (counter = 1; counter <= 10; counter++)
 	{
-		for (num = '0'; num <= 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num / 10 > 0)
 				_putchar((num / 10) + '0');
