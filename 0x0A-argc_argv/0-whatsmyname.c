964ca13e1862246e2b9af41b5c436ad734181bcb
heefCodes <abdulkareemtoyin277@gmail.com>
2022-11-13 10:35:03 +0000

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,11 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	(void)argc;
 
-	for (i = 0; i < argc; i++)
+	if (argc > 0)
 	{
-		printf("argv[%d] = %s\n", i, argv[i]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 
