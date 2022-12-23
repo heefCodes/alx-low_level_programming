0a9c83a5c91319cb4481dbe18a6cee21394ebdaf
heefCodes <abdulkareemtoyin277@gmail.com>
2022-12-23 11:58:55 +0100

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age
+ * @owner: owner of the dog
+*/
 struct dog
 {
     char *name;
     float age;
     char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
